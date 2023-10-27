812623323f920f7c3af363aa4f04ca1e743a0f49
Amath Thiam <thiamath95@gmail.com>
2023-10-27 12:06:48 -0400

    Changed app.use in index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const assert = require('assert');
-const cors = require("cors");
+// const cors = require("cors");
 const bodyParser =require('body-parser');
 require("dotenv").config({ path: "./config.env" });
 const app = express();
-app.use(cors());
+// app.use(cors());
+
+
+app.use("*", async (req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localapp.com:3000")
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+    res.setHeader("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization")
+    res.setHeader("Access-Control-Allow-Credentials", true)
+    next()
+  })
 app.use(bodyParser.json());
 
 const TicketsController = require("./controllers/tickets.controller");
@@ -33,3 +42,4 @@ app.listen(process.env.PORT, function(err){
     if (err) console.log("Error in server setup")
     console.log("Server listening on Port", process.env.PORT);
 });
+add
